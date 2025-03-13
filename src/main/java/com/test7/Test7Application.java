27122d71648849111f505b6b71061e4783896f0f
manish <manishgupta23956@gmail.com>
2025-03-13 10:40:38 +0530

    defect-fix-3456-login-error-13March

--- src/main/java/com/test7/Test7Application.java
+++ src/main/java/com/test7/Test7Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test7Application {
-
+	int x=100;
+	String u="Ram Ram";
 	public static void main(String[] args) {
 		SpringApplication.run(Test7Application.class, args);
 	}
