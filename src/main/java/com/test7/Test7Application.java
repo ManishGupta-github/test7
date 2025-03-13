788ce6ee3570ba4e2f75a4902a604b43f515c284
manish <manishgupta23956@gmail.com>
2025-03-13 12:26:00 +0530

    Modified

--- src/main/java/com/test7/Test7Application.java
+++ src/main/java/com/test7/Test7Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test7Application {
 
+
 	public static void main(String[] args) {
+		System.out.println("Hello Ji");
 		SpringApplication.run(Test7Application.class, args);
 	}
 
